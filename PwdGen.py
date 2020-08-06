b3b18c051d92eaed7fbea8a5c3e7e255220b28f7
Chirag054 <68994357+Chirag054@users.noreply.github.com>
2020-08-06 10:53:11 +0530

    Add files via upload

---
+++ PwdGen.py
@@ -0,0 +1,13 @@
+import string
+import random
+
+a: tuple = random.choice(string.ascii_letters)
+b: tuple = random.choice(string.ascii_lowercase)
+c: tuple = random.choice(string.ascii_uppercase)
+d: tuple = random.choice(string.digits)
+e: tuple = random.choice(string.punctuation)
+
+
+for i in range(7):
+    value = random.choice(a+b+c+d+e)
+    print(value,end=" ")
